cf6ac154c0a27fd54415ea53858ae481ad77f536
kuriakamau <erskinekamau@gmail.com>
2019-10-15 18:39:01 +0300

    20191014

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+# The line below will print "Hello World" to the console
+
+print("Hello World")
+
+# The line below will calculate the sum of three numbers and stores the results to variable total
+
+total=20+20+30
+print(total)
---
+++ variables.py
@@ -0,0 +1,3 @@
+# Variables are containers for storing data values for reference later in memory.
+# This memory is normally primary memory - RAM (Random Access Memory)
+# Read on data types
